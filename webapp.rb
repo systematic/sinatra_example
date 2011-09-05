a885ac418fb05e1a65cbab0533f0ffb50020131a
Matt Gordon <matt@expectedbehavior.com>
2011-09-04 22:25:26 -0400

    Changed the name of the local variable in the HAML file to emphasize that the key matches what's in the HAML file and the value is what's in the Sinatra file.

--- webapp.rb
+++ webapp.rb
@@ -2,5 +2,5 @@ require 'sinatra'
 
 get '/' do
   products = {"Soap" => "$10.00", "Baseball" => "$50.00", "Peanut Butter" => "$30.00"}
-  haml :product_listing, :locals => {:products => products}
+  haml :product_listing, :locals => {:products_to_list => products}
 end
