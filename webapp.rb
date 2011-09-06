36f7db25571f0a2cd4fcab6a19aaefe758f0c940
Matt Gordon <matt@expectedbehavior.com>
2011-09-06 15:47:39 -0400

    IN PROGRESS: We were trying to make the "Add To Cart" button work as intended.

--- webapp.rb
+++ webapp.rb
@@ -4,3 +4,7 @@ get '/' do
   products = {"Soap" => "$10.00", "Baseball" => "$50.00", "Peanut Butter" => "$30.00"}
   haml :product_listing, :locals => {:products_to_list => products}
 end
+
+post '/cart' do
+  request.inspect
+end 
